ab5083ab5aaf50dba950e71210a84d47cea7eb84
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-12-12 20:28:27 +0530

    initkal commmit

--- bynary-project/src/App.jsx
+++ bynary-project/src/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import AdminLoginPage from "./Pages/LoginPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import AllUser from "./Pages/AllUser";
+import RegistrationPage from "./Pages/RegistrationPage";
+import AdminUserPage from "./Pages/AdminUserPage";
 
 const App = () => {
   return (
     <div>
       <ToastContainer />
       <AdminLoginPage />
+      <AllUser/>
+      <RegistrationPage/>
+      <AdminUserPage/>
     </div>
   );
 };

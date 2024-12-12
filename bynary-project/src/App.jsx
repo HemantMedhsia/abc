69bace06bf80e3ff87b4f770f1b2e6b0cdacdf2c
vedansh tiwari <vt2855028@gmail.com>
2024-12-12 22:23:32 +0530

    initial commit

--- bynary-project/src/App.jsx
+++ bynary-project/src/App.jsx
@@ -2,14 +2,27 @@ import React from "react";
 import AdminLoginPage from "./Pages/LoginPage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+  createRoutesFromElements,
+} from "react-router-dom";
+import RegistrationPage from "./Pages/RegistrationPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/admin/login" element={<AdminLoginPage />} />
+      <Route path="user-register" element={<RegistrationPage />} />
+      <ToastContainer />
+    </>
+  )
+);
 
 const App = () => {
-  return (
-    <div>
-      <ToastContainer />
-      <AdminLoginPage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;

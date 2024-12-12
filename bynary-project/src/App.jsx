8726179f227fe7765ba01cb6414eba02409cbe54
Hemant Medhsia <hemantmedhsia@gmail.com>
2024-12-12 14:29:51 +0530

    initiakl commit

---
+++ bynary-project/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen flex justify-center items-center bg-teal-700 text-white text-6xl'>This is Bynry Project for the selection...</div>
+  )
+}
+
+export default App
